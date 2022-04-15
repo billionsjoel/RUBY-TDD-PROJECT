dcd66ab4d8cc1375c603ea60e1e138378ca16d49
Billions Joel <joelatugonza@gmail.com>
2022-04-16 01:10:57 +0300

    Add FIZZBUZZ method and definition

--- solver.rb
+++ solver.rb
@@ -11,4 +11,12 @@ class Solver
     word.chars.each { |char| reversed.unshift(char) }
     reversed.join
   end
+
+  def fizzbuzz(num)
+    return 'fizzbuzz' if (num % 3).zero? && (num % 5).zero?
+    return 'fizz' if (num % 3).zero?
+    return 'buzz' if (num % 5).zero?
+
+    num.to_s
+  end
 end
